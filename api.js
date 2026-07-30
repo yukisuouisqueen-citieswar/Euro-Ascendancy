4579d5e72530f8b84190515309518f2d2366b91f
yukisuouisqueen-citieswar <yukisuouisqueen@gmail.com>
2026-07-30 21:27:31 +0100

    Update api.js

--- api.js
+++ api.js
@@ -3,7 +3,7 @@
  * API communication layer connecting to your Google Apps Script Backend.gs
  */
 
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxXdun7MXhbJeGi8N4NEVpIKmvpj08UmRjiL__1kUzEyv6RV2tODxQbE89Jhmk8RK1D/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwGx6q-odlYUHDWwE9wPGvan7nYxoS_-iOKsmOqUybf7_sFXGhXfwuZeHlXpHPGiy71/exec";
 
 const API = {
   async request(params) {
